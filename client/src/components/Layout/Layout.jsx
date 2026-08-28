622e6cec4cb7b4683cd252fd27e10c0e67360a50
Kaushal Soni <kaushalsonu10101999@gmail.com>
2026-08-28 13:16:41 +0530

    Fix sidebar drifting on mobile scroll using dvh unit

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="flex bg-gray-950 light:bg-white min-h-screen">
+    <div className="flex bg-gray-950 light:bg-white min-h-dvh">
       <Sidebar open={open} setOpen={setOpen} />
       <div className={`flex-1 transition-all duration-300 ${open ? "ml-56" : "ml-16"}`}>
         <Home />
